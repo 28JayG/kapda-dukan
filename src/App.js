f2e616e9dc50da38d6ea063b71f9ec1107592e7d
28JayG <jaygandhi.32@outlook.com>
2020-01-20 13:38:36 +0530

    adds routes to menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-     <Homepage />
-    </div>
+    <Switch>
+      <Route path='/' exact component={Homepage}/>
+      <Route path='/hats' component={HatsPage}/>
+    </Switch>
   );
 }
 
