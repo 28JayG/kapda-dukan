30b6ebde90509837089aa6db032015d468a11a1a
28JayG <jaygandhi.32@outlook.com>
2020-01-21 09:55:32 +0530

    adds collection item preview for different sections

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage';
 import {Switch, Route} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop-page';
 
 function App() {
   return (
     <Switch>
       <Route path='/' exact component={Homepage}/>
-      <Route path='/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
     </Switch>
   );
 }
