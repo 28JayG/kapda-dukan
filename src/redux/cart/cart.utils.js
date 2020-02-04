dbbe3299da119eca5a6f01ef849ccf6d805fb3e8
28JayG <jaygandhi.32@outlook.com>
2020-02-05 00:57:25 +0530

    remove item from cart and add item to cart is done

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, itemToBeRemoved) => {
+  const existingItem = cartItems.find(
+    cartItem => cartItem.id === itemToBeRemoved.id
+  );
+
+  if (existingItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== existingItem.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === itemToBeRemoved.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
