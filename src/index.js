1a4d55ab5b0da3e75edd03986ca7a97bc9a4ee55
28JayG <jaygandhi.32@outlook.com>
2020-02-05 20:26:35 +0530

    adds persistence to the app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store/store';
+import { store, persister } from "./redux/store/store";
 
 import "./index.css";
 import App from "./App";
@@ -11,9 +12,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persister}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
