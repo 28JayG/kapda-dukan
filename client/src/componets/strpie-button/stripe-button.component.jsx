31338928119c8b8f6159702b7a5069bb4abe45d4
28JayG <jaygandhi.32@outlook.com>
2020-07-30 05:37:05 +0530

    bahut cheez hua hai

---
+++ client/src/componets/strpie-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from 'axios';
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_sxLf97rUFDsIVukh3HlymHl100ZZWVIes5";
+  
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      billingAddress
+      shippingAddress
+      name="Kapda Dukan"
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total price is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
---
+++ client/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { HomepageContainer } from "./homepage.styles";
+
+
+import Directory from "../../componets/directory/directory.component";
+
+const Homepage = () => (
+  <HomepageContainer>
+    <Directory />
+  </HomepageContainer>
+);
+
+export default Homepage;
